b6e51f90d92ff91fc09939cbc76c75ad1a5a170d
Rawezh Hama <Rawezh.Hama@otto.de>
2021-10-15 00:19:33 +0200

    js

---
+++ app.js
@@ -0,0 +1,27 @@
+var timer = false
+
+function satartTimer() {
+    if (!timer) {
+        let currentTime = new Date().getTime()
+        let startTime = 1000 * 60 * 5
+        let endTime = currentTime + startTime
+
+        setInterval(function () {
+            let timeLeft = endTime - new Date().getTime()
+
+            if (timeLeft > 0) {
+                let minutes = timeLeft / (1000 * 60)
+                minutes = Math.floor(minutes)
+                let seconds = (timeLeft / 1000) % 60
+                seconds = Math.round(seconds)
+                seconds = ("0" + seconds).slice(-2)
+                let text = '0' + minutes + ":" + seconds
+                document.getElementById("timer_timer").innerHTML = text
+            } else {
+                document.getElementById("timer_timer").innerHTML = "00:00"
+            }
+
+        }, 1000)
+        timer = true
+    }
+}
